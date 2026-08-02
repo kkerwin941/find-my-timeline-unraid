cae26af16c7744f760d12bdc9e2c2692d9a44883
Kevin Kerwin <kkerwin941@gmail.com>
2026-08-02 16:53:08 -0400

    Add referrerPolicy to tileLayer in app.js
    Prevents OSM titles from being blocked.

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@
   const $ = (id) => document.getElementById(id);
   const map = L.map('map', { zoomControl: false }).setView([51.1657, 10.4515], 6);
   L.control.zoom({ position: 'bottomright' }).addTo(map);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors', maxZoom: 19 }).addTo(map);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors', maxZoom: 19,referrerPolicy:'origin' }).addTo(map);
 
   const icons = { iphone: '📱', ipad: '▣', mac: '▰', watch: '⌚', airpods: '◉', default: '⌖' };
   const iconFor = (name = '') => Object.entries(icons).find(([key]) => name.toLowerCase().includes(key))?.[1] || icons.default;
